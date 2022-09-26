11428aa466e3fff3d7a0616041f5178b34bd7fdc
rio.s <rio.s101101@gmail.com>
2022-09-27 01:23:49 +0400

    Initial Commit

---
+++ SingKing/SingKingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SingKingApp.swift
+//  SingKing
+//
+//  Created by RS on 27.09.22.
+//
+
+import SwiftUI
+
+@main
+struct SingKingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
