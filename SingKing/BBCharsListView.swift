ff9bf56f36445f0814b854f504981eb2ef346d75
rio.s <rio.s101101@gmail.com>
2022-09-27 22:04:43 +0400

    List to show data fetched from API

--- SingKing/BBCharsListView.swift
+++ SingKing/BBCharsListView.swift
@@ -6,16 +6,40 @@
 //
 
 import SwiftUI
+import CachedAsyncImage
 
 struct BBCharsListView: View {
+    //TODO: Implementd Dependency Injection Swinject
+    @StateObject var viewModel = ViewModel.shared
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(viewModel.characters, id: \.self) { character in
+                    HStack {
+                        CachedAsyncImage(url: URL(string: character.imageURL), scale: 0.2) { image in
+                            image.resizable()
+                        } placeholder: {
+                            ProgressView()
+                        }
+                        .frame(width: 70, height: 80)
+                        .aspectRatio(contentMode: .fit)
+                        .cornerRadius(35)
+
+                        Spacer()
+                        
+                        Text(character.name)
+                            .bold()
+                        
+                        Spacer()
+                    }
+                }
+            }
+            .navigationTitle("BB: Characters")
+            .onAppear {
+                viewModel.getData()
+            }
         }
-        .padding()
     }
 }
 
